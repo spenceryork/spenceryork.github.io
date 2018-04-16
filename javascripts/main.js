8d6515a695bc891b84a397a1547c2570e187f8ec
Spencer York <spencerwyork@gmail.com>
2018-04-16 18:29:01 -0500

    added scrolling when nav links clicked

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,43 +1,16 @@
 "use strict";
 
-console.log("is this working");
-// const resume = require('./resume');
-
-// let blogs = [
-//     {
-//         "title": "My Blog 1",
-//         "date": "November 30th, 2017",
-//         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla enim felis, dignissim vitae nisl in, placerat blandit elit. Mauris et sapien venenatis, dapibus dui eget, egestas ante. Nam quis diam nibh. Integer nibh urna, convallis quis hendrerit vel, semper ut metus. Vestibulum auctor laoreet venenatis. Fusce pretium pharetra ipsum quis placerat. Phasellus interdum fermentum eros ac tristique. Suspendisse potenti. Nunc ut volutpat diam. Cras eleifend est lorem, vitae tincidunt magna luctus placerat. Aliquam ac ex pulvinar, vestibulum dui porta, pellentesque dolor. Aenean aliquet enim vel blandit dictum. Donec egestas egestas velit, in iaculis neque viverra nec."
-//     },
-//     {
-//         "title": "My Blog 2",
-//         "date": "December 5th, 2017",
-//         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla enim felis, dignissim vitae nisl in, placerat blandit elit. Mauris et sapien venenatis, dapibus dui eget, egestas ante. Nam quis diam nibh. Integer nibh urna, convallis quis hendrerit vel, semper ut metus. Vestibulum auctor laoreet venenatis. Fusce pretium pharetra ipsum quis placerat. Phasellus interdum fermentum eros ac tristique. Suspendisse potenti. Nunc ut volutpat diam. Cras eleifend est lorem, vitae tincidunt magna luctus placerat. Aliquam ac ex pulvinar, vestibulum dui porta, pellentesque dolor. Aenean aliquet enim vel blandit dictum. Donec egestas egestas velit, in iaculis neque viverra nec."
-//     },
-//     {
-//         "title": "My Blog 3",
-//         "date": "December 21st, 2017",
-//         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin, nisl eget interdum venenatis, nulla elit consectetur diam, in pretium tellus elit at est. Praesent dignissim tincidunt nibh. Morbi ac dignissim ex, ac vehicula ante. Nam luctus vulputate aliquam. In eget elementum tortor, nec ultricies velit. Pellentesque vitae massa eget sem scelerisque venenatis. Fusce lobortis quam risus, a tristique elit molestie sed. Curabitur dignissim velit laoreet diam dignissim sollicitudin. Nullam sit amet sem sit amet nisl aliquet blandit."
-//     },
-//     {
-//         "title": "My Blog 4",
-//         "date": "January 28th, 2018",
-//         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et tellus massa. Etiam semper fermentum elit, at pretium nulla lobortis in. Proin tincidunt justo in vestibulum ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ac tellus libero. Mauris sed lacus fermentum, vehicula velit a, tristique orci. Nulla rhoncus lacus vel enim tempus condimentum. Vivamus tellus lectus, dictum et convallis at, bibendum ut nulla. Donec in sapien ut dui interdum dictum. Nulla vel fermentum enim, nec maximus odio. Pellentesque nec risus orci. Quisque pulvinar sapien risus, eu gravida sapien mollis sed. Phasellus luctus, lacus dapibus convallis congue, sem ligula malesuada augue, eu bibendum ex erat non velit. In eros arcu, ornare ut diam in, ullamcorper porttitor leo. Etiam efficitur nisl id dui molestie, at lobortis dui finibus."
-//     },
-//     {
-//         "title": "My Blog 5",
-//         "date": "January 28th, 2018",
-//         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare mauris ex. Nam viverra urna nisi, a iaculis leo interdum vitae. Nulla euismod, neque vitae dignissim vehicula, magna nisl laoreet tortor, vitae fringilla mi leo sed diam. Cras molestie est quis sem facilisis, sit amet varius ex elementum. Sed eget elit magna. Proin porttitor, nulla ut bibendum mollis, elit augue eleifend dui, et molestie tellus quam at elit. Suspendisse cursus felis a diam dictum, quis vulputate orci eleifend. Vestibulum maximus ligula non porttitor tincidunt. Fusce vulputate arcu sit amet lobortis laoreet. Donec iaculis urna ipsum, a sollicitudin eros varius at. Donec non consequat arcu, eu iaculis diam. Donec sed diam elit. Vestibulum laoreet finibus condimentum. Vivamus finibus accumsan augue, sed tempus lorem eleifend vel. Aenean tempus convallis magna, vel varius mauris consequat sed."   
-//     }
-// ];
-
-// blogs.forEach( (blog) => {
-//     let blogHolder = document.createElement("article");
-//     blogHolder.setAttribute("class", "b-article");
-//     let blogString = `
-//     <h4>${blog.title}</h4>
-//     <p>${blog.date}</p>
-//     <p>${blog.content}</p>`;
-//     blogHolder.innerHTML = blogString;
-//     document.getElementById("blog-holder").appendChild(blogHolder);
-// });
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        console.log("what is hash", hash);
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 500, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
